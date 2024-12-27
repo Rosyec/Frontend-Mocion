a2b072fd561a328bb4cf98d9ef04223fd2b9ba47
Yosip Mike Colin <chechomens@gmail.com>
2024-12-26 19:25:19 -0500

    feat: setting primevue on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './style.css'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
+import Aura from '@primevue/themes/aura'
+
+import Menubar from 'primevue/menubar'
+import Card from 'primevue/card'
+import Button from 'primevue/button'
+import Dropdown from 'primevue/dropdown'
+import Carousel from 'primevue/carousel'
+import Paginator from 'primevue/paginator'
+import Dialog from 'primevue/dialog'
+import Tag from 'primevue/tag'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+app.component('Menubar', Menubar)
+app.component('Card', Card)
+app.component('Button', Button)
+app.component('Dropdown', Dropdown)
+app.component('Carousel', Carousel)
+app.component('Paginator', Paginator)
+app.component('Dialog', Dialog)
+app.component('Tag', Tag)
 
 app.use(createPinia())
 app.use(router)
